b9e58f330cddab181d7340550dd58d39143f7545
Dinh Luu <dinhluu@server.local>
2016-06-23 10:12:52 +0700

    add helper rect function

--- LearnDrawing/LearnDrawing/CGRect+Extensiom.swift
+++ LearnDrawing/LearnDrawing/CGRect+Extensiom.swift
@@ -14,3 +14,44 @@ extension CGRect {
     return CGPoint(x: CGRectGetMidX(self), y: CGRectGetMidY(self))
   }
 }
+
+func rectAroundCenter(center: CGPoint, size: CGSize) -> CGRect {
+  let halfWidth = size.width / 2
+  let halfHeight = size.height / 2
+  
+  return CGRect(x: center.x - halfWidth, y: center.y - halfHeight, width: size.width, height: size.height)
+}
+
+func sizeScaleByFactor(size: CGSize, factor: CGFloat) -> CGSize {
+  return CGSize(width: size.width * factor, height: size.height * factor)
+}
+
+// ---------- DRAWING FITTING---------
+func aspectScaleFit(sourceSize: CGSize, desRect: CGRect) -> CGFloat {
+  let desSize = desRect.size
+  let scaleW = desSize.width / sourceSize.width
+  let scaleH = desSize.height / sourceSize.height
+  return min(scaleW, scaleH)
+}
+
+func rectByFittingInRect(sourceRect: CGRect, destRect: CGRect) -> CGRect {
+  let aspect = aspectScaleFit(sourceRect.size, desRect: destRect)
+  let targetSize = sizeScaleByFactor(sourceRect.size, factor: aspect)
+  return rectAroundCenter(destRect.dl_center(), size: targetSize)
+}
+
+// ---------- DRAWING FILLING--------
+func aspectScaleFill(sourceSize: CGSize, destRect: CGRect) -> CGFloat {
+  let destSize = destRect.size
+  let scaleW = destSize.width / sourceSize.width
+  let scaleH = destSize.height / sourceSize.height
+  return max(scaleW, scaleH)
+}
+
+func rectByFillingRect(sourceRect: CGRect, destRect: CGRect) -> CGRect {
+  let aspect = aspectScaleFill(sourceRect.size, destRect: destRect)
+  let targetSize = sizeScaleByFactor(sourceRect.size, factor: aspect)
+  return rectAroundCenter(destRect.dl_center(), size: targetSize)
+}
+
+
